822d4188e579607918da3f4cd9516dbb330a130e
Lisa Olson <olson.lisa94@gmail.com>
2016-09-08 14:51:09 -0700

    Adds AJAX call and render function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,25 @@
 console.log('app.js is loaded');
+
+$(document).ready(function() {
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/events',
+    success: renderEvents
+  });
+
+  function renderEvents(events) {
+    events.forEach(function(e) {
+      renderEvent(e);
+    });
+  }
+
+  function renderEvent(e) {
+    console.log('rendering event', e);
+    var eventHtml = $('#event-template').html();
+    var eventsTemplate = Handlebars.compile(eventHtml);
+    var html = eventsTemplate(e);
+    $('#eventTarget').prepend(html);
+  }
+
+})
