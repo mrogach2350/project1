048b9edffc417c256f56b077edd2e7bf15b3b02f
mrogach2350 <m.rogach@gmail.com>
2016-09-08 16:42:56 -0700

    fixes templating and rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,24 +2,37 @@ console.log('app.js is loaded');
 
 $(document).ready(function() {
 
+
   $.ajax({
     method: 'GET',
     url: '/api/events',
     success: renderEvents
   });
 
-  function renderEvents(events) {
-    events.forEach(function(e) {
-      renderEvent(e);
-    });
+  function renderEvents(json){
+    allEvents = json;
+    renderEvent();
   }
 
-  function renderEvent(e) {
-    console.log('rendering event', e);
+  function renderEvent(){
     var eventHtml = $('#event-template').html();
     var eventsTemplate = Handlebars.compile(eventHtml);
-    var html = eventsTemplate(e);
+    var html = eventsTemplate({events:allEvents});
     $('#eventTarget').prepend(html);
   }
+
+  // function renderEvents(events) {
+  //   events.forEach(function(e) {
+  //     renderEvent(e);
+  //   });
+  // }
+  //
+  // function renderEvent(e) {
+  //   console.log('rendering event', e);
+  //   var eventHtml = $('#event-template').html();
+  //   var eventsTemplate = Handlebars.compile(eventHtml);
+  //   var html = eventsTemplate({events:e});
+  //   $('#eventTarget').prepend(html);
+  // }
 
 })
