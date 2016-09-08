292f9eba5bf7d8dd7bbccfd916ed9a22e0cd2e13
mrogach2350 <m.rogach@gmail.com>
2016-09-08 14:28:52 -0700

    adds server.js boilerplate and index route

---
+++ server.js
@@ -0,0 +1,64 @@
+// require express and other modules
+var express = require('express'),
+    app = express();
+
+// parse incoming urlencoded form data
+// and populate the req.body object
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true}));
+
+// allow cross origin requests (optional)
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/************
+ * DATABASE *
+ ************/
+
+var db = require('./models');
+
+/**********
+ * ROUTES *
+ **********/
+
+ // Serve static files from the `/public` directory:
+ // i.e. `/images`, `/scripts`, `/styles`
+ app.use(express.static('public'));
+
+ /*
+ * HTML Endpoints
+ */
+
+ app.get('/', function homepage(req, res) {
+   console.log(__dirname + '/views/index.html');
+   res.sendFile(__dirname + '/views/index.html');
+ });
+
+ /*
+  * JSON API Endpoints
+  */
+
+app.get('/api/events', function api_event(req, res){
+  db.Event.find({})
+    .exec(function(err, events){
+      if (err){
+        res.status(500).send(err);
+        return;
+      }
+      res.json(events);
+    });
+});
+
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
