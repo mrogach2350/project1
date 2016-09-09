b9a18b0632f2a069c39c5bb17caeb2676ce96ac2
mrogach2350 <m.rogach@gmail.com>
2016-09-08 20:22:58 -0700

    adds render dropdown plugin

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,8 @@
 console.log('app.js is loaded');
 
-$(document).ready(function() {
 
+
+$(document).ready(function() {
 
   $.ajax({
     method: 'GET',
@@ -19,6 +20,16 @@ $(document).ready(function() {
     var eventsTemplate = Handlebars.compile(eventHtml);
     var html = eventsTemplate({events:allEvents});
     $('#eventTarget').prepend(html);
+    $('.dropdown-button').dropdown({
+       inDuration: 300,
+       outDuration: 225,
+       constrain_width: false, // Does not change width of dropdown to that of the activator
+       hover: true, // Activate on hover
+       gutter: 0, // Spacing from edge
+       belowOrigin: false, // Displays dropdown below the button
+       alignment: 'left' // Displays dropdown with edge aligned to the left of button
+     }
+    );
   }
 
   // function renderEvents(events) {
