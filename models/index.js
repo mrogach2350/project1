f961df12ce859ab102cee9229b12d7a6ff3765e0
mrogach2350 <m.rogach@gmail.com>
2016-09-12 15:06:13 -0700

    fixes spelling

--- models/index.js
+++ models/index.js
@@ -2,4 +2,4 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/events");
 
 module.exports.Event = require('./event.js');
-moduel.exports.Person = require('./person.js');
+module.exports.Person = require('./person.js');
