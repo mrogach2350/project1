909780e15278684ee2613c32d6ed718dcdc4a6f8
mrogach2350 <m.rogach@gmail.com>
2016-09-12 21:19:47 -0700

    adds validation to event model

--- models/event.js
+++ models/event.js
@@ -2,11 +2,16 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var EventSchema = new Schema({
-  name: String,
-  host: String,
-  where: String,
-  when: String,
-  what: String
+  name: {String,
+  required: true},
+  host: {String,
+  required: true},
+  where: {String,
+  required: true},
+  when: {String,
+  required: true},
+  what: {String,
+  required: true}
 });
 
 var Event = mongoose.model('Event', EventSchema);
